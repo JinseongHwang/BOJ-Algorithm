5ba1b23cc704d007aba713251ad4c54f0589c4f9
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-06-20 04:54:16 +0900

    Yonsei TOTO

---
+++ 12018.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void init() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+}
+
+int main() {
+	int numSubject, ownMileage; cin >> numSubject >> ownMileage;
+	vector<int> minMileage;
+	for (int i = 0; i < numSubject; ++i) {
+		int request, limit; cin >> request >> limit;
+		vector<int> v;
+		for (int currMileage, j = 0; j < request; ++j) {
+			cin >> currMileage;
+			v.push_back(currMileage);
+		}
+		sort(v.begin(), v.end(), greater<int>());
+		int currMin = (v.size() < limit) ? v.back() : v[limit - 1];
+		if (request < limit) { currMin = 1; } // 질문글 보고 추가한 내용
+		minMileage.push_back(currMin);
+	}
+	sort(minMileage.begin(), minMileage.end());
+	int sum = 0, result = 0;
+	for (int i = 0; i < minMileage.size(); ++i) {
+		if (ownMileage - minMileage[i] >= 0) {
+			ownMileage -= minMileage[i];
+			result++;
+		}
+		else break;
+	} 
+	cout << result << "\n";
+
+	return 0;
+}
