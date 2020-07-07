e2e47b2cd9de20c51b1378992dd6d0d70ecbaa2e
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-07-07 17:33:48 +0900

    연결 요소의 개수

--- 11724.cpp
+++ 11724.cpp
@@ -35,3 +35,44 @@ int main() {
 	cout << result << "\n";
 	return 0;
 }
+
+
+// 2020-07-07 추가
+#include <iostream>
+using namespace std;
+
+#define MAX 1001
+bool graph[MAX][MAX], visit[MAX], foundComponent;
+int V, E, result;
+
+void init() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+}
+
+void dfs(int curr) {
+	if (visit[curr]) return;
+	visit[curr] = true; foundComponent = true;
+	for (int i = 1; i <= V; ++i) {
+		if (graph[curr][i] && !visit[i]) dfs(i);
+	}
+}
+
+int main() {
+	init();
+	cin >> V >> E;
+	for (int a, b, i = 0; i < E; ++i) {
+		cin >> a >> b;
+		graph[a][b] = true;
+		graph[b][a] = true;
+	}
+	// 문제에서 정점의 번호가 1부터 주어져서 0은 무시
+	for (int i = 1; i <= V; ++i) {
+		foundComponent = false;
+		dfs(i);
+		if (foundComponent) result++;
+	}
+	cout << result << "\n";
+
+	return 0;
+}
