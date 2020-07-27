dc7794cd6aad4491dd748e2528ee3b6f3ed7f198
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-07-27 20:10:18 +0900

    카드 정렬하기

--- 1715.cpp
+++ 1715.cpp
@@ -33,3 +33,45 @@ int main() {
 
 	return 0;
 }
+
+
+// =========
+// 2020-07-27 추가
+
+#include <iostream>
+#include <queue>
+#include <functional>
+using namespace std;
+
+void init() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+}
+
+int main() {
+	init();
+	priority_queue<int, vector<int>, greater<int> > pq;
+	int n; cin >> n;
+	if (n == 1) {
+		cout << "0\n";
+		return 0;
+	}
+
+	for (int inp, i = 0; i < n; ++i) {
+		cin >> inp;
+		pq.push(inp);
+	}
+
+	int result = 0;
+	while (1) {
+		int a = pq.top(); pq.pop();
+		int b = pq.top(); pq.pop();
+		int sum = a + b;
+		result += sum;
+		if (pq.empty()) break;
+		pq.push(sum);
+	}
+	cout << result << '\n';
+
+	return 0;
+}
