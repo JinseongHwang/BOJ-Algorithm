b12668c46b433261bde8b3a9f8943f4f37b7023b
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-07-28 16:46:40 +0900

    여행 가자

--- 1976.cpp
+++ 1976.cpp
@@ -50,3 +50,54 @@ bool find_parent(int a, int b) {
 	if (get_parent(a) == get_parent(b)) return true;
 	else return false;
 }
+
+
+// 2020-07-28 추가
+#include <iostream>
+using namespace std;
+
+const int MAX = 1001;
+int n, m, parent[MAX];
+
+void init() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	// 자기 자신이 부모로 초기화
+	for (int i = 0; i < MAX; ++i) parent[i] = i;
+}
+
+int getParent(int c) { // 조상 부모를 반환
+	if (parent[c] == c) return c;
+	return parent[c] = getParent(parent[c]);
+}
+
+void unionParent(int a, int b) { // 두 그래프를 합한다
+	int ap = getParent(a);
+	int bp = getParent(b);
+	ap < bp ? parent[bp] = ap : parent[ap] = bp;
+}
+
+int main() {
+	init();
+	cin >> n >> m;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			int inp; cin >> inp;
+			// 양방향으로 주어지기 때문에 필요없는 한쪽은 버린다
+			if (j < i) continue;
+			if (inp) unionParent(i, j); // 합
+		}
+	}
+	int tmp; cin >> tmp;
+	int symbol = getParent(tmp); // 최초 부모랑
+	for (int i = 0; i < m - 1; ++i) {
+		cin >> tmp;
+		if (symbol != getParent(tmp)) { // 다른 노드 부모랑 다르면 NO 하고 끝냄
+			cout << "NO\n"; return 0;
+		}
+	}
+	cout << "YES\n"; // 안끝났다면 YES
+
+	return 0;
+}
