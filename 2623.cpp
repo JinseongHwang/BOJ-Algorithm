921fa1db8e148fe7d343d8f75062b5095a923df7
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-07-29 16:48:20 +0900

    음악 프로그램

--- 2623.cpp
+++ 2623.cpp
@@ -63,3 +63,67 @@ int main() {
 
 	return 0;
 }
+
+
+// 2020-07-29 추가
+// 음악프로그램
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+const int MAX = 1001;
+int n, query, indegree[MAX];
+vector<int> v[MAX], result;
+queue<int> q;
+
+void init() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+}
+
+void topologySort() {
+	for (int i = 1; i <= n; ++i) {
+		// 진입차수가 0인 정점을 queue에 push
+		if (!indegree[i]) q.push(i);
+	}
+	for (int i = 1; i <= n; ++i) {
+		if (q.empty()) { // 사이클이 발생한 경우
+			cout << "0\n";
+			return;
+		}
+		// queue에서 하나씩 꺼내서 result에 삽입
+		int curr = q.front();
+		q.pop();
+		result.push_back(curr);
+		// 꺼낸 정점을 삭제하는 연산
+		// 정점이 삭제됨에 따라 받는 정점들의 진입 차수 감소
+		// 감소 후 진입차수가 0이 되는 정점을 다시 queue에 push
+		for (auto a : v[curr]) {
+			indegree[a]--;
+			if (!indegree[a]) q.push(a);
+		}
+	}
+	// 결과값 출력
+	for (const auto& res : result) cout << res << '\n';
+}
+
+int main() {
+	init();
+	cin >> n >> query; // 가수의 수, 질의 수
+	while (query--) {
+		int tmp; cin >> tmp; // 순서 개수
+		vector<int> inp; // 필요한 순서 값들을 저장
+		for (int a, i = 0; i < tmp; ++i) {
+			cin >> a; inp.push_back(a);
+			if (i > 0) indegree[a]++; // 진입차수 setting
+		}
+		for (int i = inp.size() - 1; i > 0; --i) {
+			v[inp[i - 1]].push_back(inp[i]); // 순서 정보 setting
+		}
+	}
+	topologySort(); // 위상정렬 및 출력
+
+	return 0;
+}
