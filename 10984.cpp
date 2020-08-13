83ddc801b86b6fd6807d25e16aeb5e4d9a4f22bb
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-08-13 21:06:40 +0900

    내 학점을 구해줘

--- 10984.cpp
+++ 10984.cpp
@@ -24,3 +24,24 @@ int main() {
 
 	return 0;
 }
+
+
+
+// C언어 io 함수로 구현
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int sem; scanf("%d", &sem);
+	for (int i = 0; i < sem; ++i) {
+		int nSub; scanf("%d", &nSub);
+		int sum1 = 0; double sum2 = 0;
+		for (int j = 0; j < nSub; ++j) {
+			int u; scanf("%d", &u); sum1 += u;
+			double v; scanf("%lf", &v); sum2 += (double)(u * v);
+		}
+		printf("%d %.1lf\n", sum1, (double)sum2 / sum1);
+	}
+	return 0;
+}
