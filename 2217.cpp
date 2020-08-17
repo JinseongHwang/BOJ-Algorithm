ced4206bd9f03a5ce43d8f9e5ed74669d8d75bde
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-08-17 13:26:48 +0900

    로프

--- 2217.cpp
+++ 2217.cpp
@@ -20,3 +20,28 @@ int main() {
 	cout << result << "\n";
 	return 0;
 }
+
+// 2020-08-17 추가
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, result = 0, arr[100010] = { 0, };
+	cin >> n;
+	for (int i = 0; i < n; ++i) cin >> arr[i];
+	sort(arr, arr + n, greater<int>());
+
+	for (int i = 0; i < n; ++i) {
+		int nrope = i + 1;
+		int curr = arr[i] * nrope;
+		if (result <= curr) result = curr;
+	}
+	cout << result << "\n";
+
+	return 0;
+}
