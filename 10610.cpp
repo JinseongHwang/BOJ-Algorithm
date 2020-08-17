9b34847c0363246a8e7ebfee380531cfa13a6fe6
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-08-17 14:45:54 +0900

    30

--- 10610.cpp
+++ 10610.cpp
@@ -32,3 +32,33 @@ int main() {
 
 	return 0;
 }
+
+
+// 2020-08-17 추가
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	const int MAX = 1e5;
+	string s; cin >> s;
+
+	int sum = 0; bool zeroExist = false;
+	for (int i = 0; i < s.length(); ++i) {
+		sum += s[i] - '0';
+		if (s[i] == '0') zeroExist = true;
+	}
+	if (zeroExist && sum % 3 == 0) {
+		sort(s.begin(), s.end(), greater<>());
+		cout << s << "\n";
+	}
+	else {
+		cout << "-1\n";
+	}
+	return 0;
+}
