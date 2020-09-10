9063d4f7e724583e152ead68fc2a0771b441b65a
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-09-11 03:42:16 +0900

    N과 M(1)

--- 15649.cpp
+++ 15649.cpp
@@ -34,3 +34,35 @@ int main() {
 
 	return 0;
 }
+
+
+// 2020-09-11 추가
+
+#include <cstdio>
+using namespace std;
+
+int n, m, arr[10];
+bool isused[10];
+
+void solve(int k) { // 현재 k개의 수가 완성되었다.
+	if (m == k) { // 기저사례: m개의 수를 채웠으면 return;
+		for (int i = 0; i < m; ++i) printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		if (!isused[i]) {
+			arr[k] = i;
+			isused[i] = true;
+			solve(k + 1);
+			isused[i] = false;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	solve(0);
+	return 0;
+}
