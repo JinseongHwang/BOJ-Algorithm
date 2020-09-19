85a00a3c7be23def60eb73b4a329a7622be50d04
JinseongHwang <52629158+JinseongHwang@users.noreply.github.com>
2020-09-19 19:00:21 +0900

    N과 M(2)

--- 15650.cpp
+++ 15650.cpp
@@ -37,3 +37,35 @@ int main() {
 	dfs(0);
 	return 0;
 }
+
+
+// 2020-09-19 추가
+#include <cstdio>
+using namespace std;
+
+int n, m, arr[10];
+bool isused[10];
+
+void solve(int k) {
+	if (m == k) {
+		for (int i = 1; i < m; ++i) if (arr[i - 1] >= arr[i]) return; // 오름차순 검사
+		for (int i = 0; i < m; ++i) printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i <= n; ++i) {
+		if (!isused[i]) {
+			arr[k] = i;
+			isused[i] = true;
+			solve(k + 1);
+			isused[i] = false;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &n, &m);
+	solve(0);
+	return 0;
+}
