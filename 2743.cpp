3d7c99851238445f859d7f56b5ed8b1e63f0ede7
JinseongHwang <eddy5360@naver.com>
2021-01-07 01:10:38 +0900

    단어 길이 재기

---
+++ 2743.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	string input; cin >> input; cout << input.length() << "\n";
+	return 0;
+}
